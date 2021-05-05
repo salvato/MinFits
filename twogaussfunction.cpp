d55e10c8d55110294e5de965de52517dee0fd3a6
Gabriele Salvato <gabriele.salvato@cnr.it>
2021-05-05 06:28:40 +0200

    da completare twogaussfunction.cpp

--- twogaussfunction.cpp
+++ twogaussfunction.cpp
@@ -127,22 +127,19 @@ TwoGaussFunction::readDataFile() {
 double
 TwoGaussFunction::operator()(const std::vector<double>& par) const {
     Q_UNUSED(par);
-//    double Beta,  VM,  V0,  Tau, Flow1;
-//    double Beta1, VM1, V01, Tau1, Flow2;
-//    double STS;
     double f = 0.0;
 
-//    Beta  = par[0]*1.0e-3;
-//    VM    = par[1];
-//    V0    = par[2];
-//    Tau   = par[3]*1.0e-14;
-//    Flow1 = par[4];
-//    VM1   = par[5];
-//    V01   = par[6];
-//    Beta1 = par[7]*1.0e-3;
-//    Tau1  = par[8]*1.0e-14;
-//    Flow2 = par[9];
-//    STS   = 0.0;
+    Beta  = par[0]*1.0e-3;
+    Vm    = par[1];
+    V0    = par[2];
+    Tau   = par[3]*1.0e-14;
+    Flow1 = par[4];
+    Vm1   = par[5];
+    V01   = par[6];
+    Beta1 = par[7]*1.0e-3;
+    Tau1  = par[8]*1.0e-14;
+    Flow2 = par[9];
+    sts   = 0.0;
 
 //    double Diff;
 //    for(int j=0; j<theMeasurements.size(); j++) {
@@ -159,40 +156,7 @@ TwoGaussFunction::operator()(const std::vector<double>& par) const {
 //      Diff1[j] = Diff;
 //      f      += Diff*Diff;
 //    }
-/*
-    if(iflag==3 || !div(nCall,100).rem) {
-      if(pPlotA != NULL) {
-        pPlotA->DelDataSet(1);
-        pPlotA->DelDataSet(2);
-        pPlotA->NewDataSet(1, 1, RGB(255,0,0), CPlotWindow::iline, "Valori Teorici");
-        pPlotA->NewDataSet(2, 1, RGB(255,255,0), CPlotWindow::iline, "Dati Sperimentali");
-        for(int i=0; i<nDati; i++) {
-          pPlotA->NewPoint(1, T[i], Alfa[i]);
-          pPlotA->NewPoint(2, T[i], AlfaS[i]);
-        }
-        pPlotA->SetShowTitle(1, true);
-        pPlotA->SetShowTitle(2, true);
-        pPlotA->SetShowDataSet(1, true);
-        pPlotA->SetShowDataSet(2, true);
-        pPlotA->UpdatePlot();
-      }
-      if(pPlotV != NULL) {
-        pPlotV->DelDataSet(1);
-        pPlotV->DelDataSet(2);
-        pPlotV->NewDataSet(1, 1, RGB(255,0,0), CPlotWindow::iline, "Alfa 1");
-        pPlotV->NewDataSet(2, 1, RGB(255,255,0), CPlotWindow::iline, "Alfa 2");
-        for(int i=0; i<nDati; i++) {
-          pPlotV->NewPoint(1, T[i], Alfa1[i]);
-          pPlotV->NewPoint(2, T[i], Alfa2[i]);
-        }
-        pPlotV->SetShowTitle(1, true);
-        pPlotV->SetShowTitle(2, true);
-        pPlotV->SetShowDataSet(1, true);
-        pPlotV->SetShowDataSet(2, true);
-        pPlotV->UpdatePlot();
-      }
-    }
-*/
+
     return f;
 }
 
@@ -200,21 +164,27 @@ TwoGaussFunction::operator()(const std::vector<double>& par) const {
 void
 TwoGaussFunction::Plot(const std::vector<double>& par) const
 {
-//    Beta1 = par[0];
-//    Beta2 = par[1];
-//    Tm    = par[2]*1.0e+03;
-//    Tau   = par[3]*1.0e-14;
-//    T00   = par[4];
+    Beta  = par[0]*1.0e-3;
+    Vm    = par[1];
+    V0    = par[2];
+    Tau   = par[3]*1.0e-14;
+    Flow1 = par[4];
+    Vm1   = par[5];
+    V01   = par[6];
+    Beta1 = par[7]*1.0e-3;
+    Tau1  = par[8]*1.0e-14;
+    Flow2 = par[9];
+    sts   = 0.0;
 
-    double summa;
-    double eps  = 1.0e-15;
-    int maxIter = 300;
-    int maxterm = 100;
-    for(ulong j=0; j<theMeasurements.size(); j++) {
+//    double summa;
+//    double eps  = 1.0e-15;
+//    int maxIter = 300;
+//    int maxterm = 100;
+//    for(ulong j=0; j<theMeasurements.size(); j++) {
 //        T1 = theTemperatures[j];
 //        dceul(summTerm, eps, maxIter, maxterm, &summa);
 //        theFit[j] =  Beta1*summa;
-    }
+//    }
 
     if(pPlot) {
         pPlot->ClearDataSet(1);
